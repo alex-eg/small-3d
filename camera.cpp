5ed0c14eeaf49e8f06a64bf12b5e57e4715ae6e5
taptap <alexey.e.egorov@gmail.com>
2012-11-29 10:49:41 +0000

    added sort of smooth moving to camera

--- camera.cpp
+++ camera.cpp
@@ -2,13 +2,12 @@
 
 realCamera::realCamera()
 {
-    up = glm::vec3(0.0, -1.0, 0.0);
-    position = glm::vec3(0.0, 1.0, 1.0);
+    up = glm::vec3(0.0, 1.0, 0.0);
+    position = glm::vec3(0.0, 0.0, 2.0);
     target = glm::vec3(0.0, 0.0, 0.0);
-    fovy = 50.0;
-    aspect = 1.0;
 
-    mv = glm::lookAt(position, target, up);
+    modelView = glm::lookAt(position, target, up);
+    projection = glm::perspective(fovy, aspect, zNear, zFar);
 }
 
 realCamera::~realCamera()
@@ -18,34 +17,51 @@ realCamera::~realCamera()
 void realCamera::setUpVector(glm::vec3 newUp)
 {
     up = newUp;
-    updateModelViewMatrix();
+    updateMatrices();
 }
 
 void realCamera::setPositionVector(glm::vec3 newPosition)
 {
     position = newPosition;
-    updateModelViewMatrix();
+    updateMatrices();
 }
 
 void realCamera::setTargetVector(glm::vec3 newTarget)
 {
     target = newTarget;
-    updateModelViewMatrix();
+    updateMatrices();
 }
 
-void realCamera::updateModelViewMatrix()
+void realCamera::updateMatrices()
 {
+    glMatrixMode(GL_PROJECTION);
+    projection = glm::perspective(fovy, aspect, zNear, zFar);
+    glLoadMatrixf(&projection[0][0]);
+
     glMatrixMode(GL_MODELVIEW);
-    mv = glm::lookAt(position, target, up);
-    glLoadMatrixf(&mv[0][0]);
+    modelView = glm::lookAt(position, target, up);
+    glLoadMatrixf(&modelView[0][0]);
 }
 
-glm::mat4 realCamera::getModelViewMatrix()
+glm::mat4 realCamera::getModelViewMatrix() const
 {
-    return mv;
+    return modelView;
+}
+
+glm::mat4 realCamera::getProjectionMatrix() const
+{
+    return projection;
+}
+
+void realCamera::render()
+{
+    updateMatrices();
 }
 
 /* Flying camera class */
+
+float flyingCamera::maxMoveSpeed = 3.0f;
+float flyingCamera::maxRotateSpeed = 10.0f;
 
 flyingCamera::flyingCamera() : realCamera() {};
 flyingCamera::~flyingCamera() {};
@@ -60,7 +76,7 @@ void flyingCamera::rotatePitch(float degrees)
     up = rot3 * up;
     glm::vec3 newDir = rot3 * direction;
     target = newDir + position;
-    updateModelViewMatrix();
+    updateMatrices();
 }
 
 void flyingCamera::rotateYaw(float degrees)
@@ -72,7 +88,7 @@ void flyingCamera::rotateYaw(float degrees)
     glm::mat3 rot3(rot);
     glm::vec3 newDir = rot3 * direction;
     target = newDir + position;
-    updateModelViewMatrix();
+    updateMatrices();
 }
 
 void flyingCamera::rotateRoll(float degrees)
@@ -85,7 +101,7 @@ void flyingCamera::moveForward(float meters)
     glm::vec3 direction = target - position;
     direction = glm::normalize(direction);
     position += direction * meters;
-    updateModelViewMatrix();
+    updateMatrices();
 }
 
 void flyingCamera::moveSide(float meters)
@@ -94,12 +110,75 @@ void flyingCamera::moveSide(float meters)
     direction = glm::normalize(direction);
     glm::vec3 side = glm::cross(direction, up);
     position += side * meters;
-    updateModelViewMatrix();
+    updateMatrices();
 }
 
 void flyingCamera::moveVertical(float meters)
 {
     glm::vec3 normUp = glm::normalize(up);
     position += normUp * meters;
-    updateModelViewMatrix();
+    updateMatrices();
+}
+
+void flyingCamera::updatePosition() // Dynamics have to be in another componemt!
+{
+    moveForward(velocityForward);
+    moveSide(velocitySide);
+    moveVertical(velocityVertical);
+
+    rotateRoll(velocityRoll);
+    rotatePitch(velocityPitch);
+    rotateYaw(velocityYaw);
+
+    //velocityYaw -= .1;
+    //velocityPitch -= .1;
+    //velocityRoll -= .1;
+
+    //velocityForward -= .1;
+    //velocitySide -= .1;
+    //velocityVertical -= .1;
+}
+
+// Todo: move dynamics in another class! Or even PROPERTY
+// everyone likes property-based engines!
+// Keywords: message passing,
+
+void flyingCamera::addForwardSpeed(float meters_per_second)
+{
+    velocityForward += meters_per_second;
+    if ((velocityForward >  maxMoveSpeed) || 
+	(velocityForward < -maxMoveSpeed)) velocityForward -= meters_per_second;
+}
+void flyingCamera::addSideSpeed(float meters_per_second)
+{
+    velocitySide += meters_per_second;
+    if ((velocitySide >  maxMoveSpeed) || 
+	(velocitySide < -maxMoveSpeed)) velocitySide -= meters_per_second;
+}
+void flyingCamera::addVerticalSpeed(float meters_per_second)
+{
+    velocityVertical += meters_per_second;
+    if ((velocityVertical >  maxMoveSpeed) || 
+	(velocityVertical < -maxMoveSpeed)) velocityVertical -= meters_per_second;
+}
+
+void flyingCamera::addYawSpeed(float degrees_per_second)
+{
+    velocityYaw += degrees_per_second;
+    if ((velocityYaw >  maxRotateSpeed) ||
+	(velocityYaw < -maxRotateSpeed)) velocityYaw -= degrees_per_second;
+}
+
+void flyingCamera::addPitchSpeed(float degrees_per_second)
+{
+    velocityPitch += degrees_per_second;
+    if ((velocityPitch >  maxRotateSpeed) ||
+	(velocityPitch < -maxRotateSpeed)) velocityPitch -= degrees_per_second;
+}
+
+void flyingCamera::addRollSpeed(float degrees_per_second)
+{
+    velocityRoll += degrees_per_second;
+    if ((velocityRoll >  maxRotateSpeed) ||
+	(velocityRoll < -maxRotateSpeed)) velocityRoll -= degrees_per_second;
 }
--- camera.hpp
+++ camera.hpp
@@ -5,7 +5,7 @@
 #include <glm/glm.hpp>
 #include <GL/gl.h>
 
-#define PI 3.1415927
+#define PI 3.14159265
 
 // Interface class
 class camera {
@@ -13,11 +13,11 @@ public:
     virtual void setUpVector(glm::vec3 newUp)         = 0;
     virtual void setPositionVector(glm::vec3 newPos)  = 0;
     virtual void setTargetVector(glm::vec3 newTarget) = 0;
-    
+						
     virtual void rotatePitch(float degrees) = 0;
     virtual void rotateYaw(float degrees)   = 0;
     virtual void rotateRoll(float degrees)  = 0;
-					   
+
     virtual void moveForward(float meters)  = 0;
     virtual void moveSide(float meters)     = 0;
     virtual void moveVertical(float meters) = 0;
@@ -25,14 +25,18 @@ public:
 
 class realCamera : public camera {
 protected:
-    glm::mat4 mv;
+    glm::mat4 modelView;
+    glm::mat4 projection;
     glm::vec3 up;
     glm::vec3 position;
     glm::vec3 target;
-    float fovy;
-    float aspect;
 
-    void updateModelViewMatrix();
+    float fovy = 60.0;
+    float aspect = 1.0;
+    float zNear = 0.1;
+    float zFar = 99.0;
+
+    void updateMatrices();
 public:
     realCamera();
     ~realCamera();
@@ -41,21 +45,45 @@ public:
     void setPositionVector(glm::vec3 newPos);
     void setTargetVector(glm::vec3 newTarget);
 
-    glm::mat4 getModelViewMatrix();
+    void render();
+
+    glm::mat4 getModelViewMatrix() const;
+    glm::mat4 getProjectionMatrix() const;
 };
 
 class flyingCamera : public realCamera {
-public:
-    flyingCamera();
-    ~flyingCamera();
+private:
+    static float maxMoveSpeed;
+    static float maxRotateSpeed;
 
-    void rotatePitch(float degrees);
+    float velocityForward = 0;
+    float velocitySide = 0;
+    float velocityVertical = 0;
+
+    float velocityYaw = 0;
+    float velocityPitch = 0;
+    float velocityRoll = 0;
+
     void rotateYaw(float degrees);
+    void rotatePitch(float degrees);
     void rotateRoll(float degrees);
 
     void moveForward(float meters);
     void moveSide(float meters);
     void moveVertical(float meters);
+public:
+    flyingCamera();
+    ~flyingCamera();
+
+    void updatePosition();
+
+    void addForwardSpeed(float meters_per_second);
+    void addSideSpeed(float meters_per_second);
+    void addVerticalSpeed(float meters_per_second);
+
+    void addYawSpeed(float degrees_per_second);
+    void addPitchSpeed(float degrees_per_second);
+    void addRollSpeed(float degrees_per_second);
 };
 
 #endif // ifndef CAMERA
